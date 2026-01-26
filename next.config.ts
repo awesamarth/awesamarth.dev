a05ce33c999ea36be9efa539d8cfe2fce1b0c72a
Samarth Saxena <samarthsaxena1672003@gmail.com>
2026-01-26 22:31:02 +0530

    update next version

--- next.config.ts
+++ next.config.ts
@@ -12,19 +12,6 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
-
-    // Ignore optional ws deps
-    config.ignoreWarnings = [
-      { module: /node_modules\/ws\/lib\// }
-    ];
-    return config;
-  },
 };
 
 export default nextConfig;
